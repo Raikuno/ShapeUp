c2305e73eafb615e828d4908a57fc2675ea31455
jpersan111 <jpersan111@g.educaand.es>
2024-04-16 10:30:08 +0200

    Ser del aberno añadido

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody3D
 
 func _ready():
-	# 0 = ameba / 1 = cilindro / 2 = cubo / 3 = esfera / 4 = peakamide
-	var head = randi_range(0,4)
-	var body = randi_range(0,4)
-	var leftArm = randi_range(0,4)
-	var rightArm = randi_range(0,4)
-	var legs = randi_range(0,4)
+	# 0 = amebaGOD / 1 = cilindro / 2 = cubo / 3 = esfera / 4 = peakamide / 5 = amebaEvil
+	var head = randi_range(0,5)
+	var body = randi_range(0,5)
+	var leftArm = randi_range(0,5)
+	var rightArm = randi_range(0,5)
+	var legs = randi_range(0,5)
 	match head:
 		0:
 			$"pivot/head/cabeza-amebaPlayer".show()
@@ -18,6 +18,8 @@ func _ready():
 			$"pivot/head/cabeza-esferaPlayer".show()
 		4:
 			$"pivot/head/cabeza-piramidePlayer".show()
+		5:
+			$"pivot/head/cabeza-amebaPlayerMK2".show()
 	match body:
 		0:
 			$"pivot/body/cuerpo-amebaPlayer".show()
@@ -29,6 +31,8 @@ func _ready():
 			$"pivot/body/cuerpo-piramidePlayer".show()
 		4:
 			$"pivot/body/esfera-CuerpoPlayer".show()
+		5:
+			$"pivot/body/cuerpo-amebaPlayerMKII".show()
 	match leftArm:
 		0:
 			$"pivot/leftArm/brazo-amebaPlayer".show()
@@ -40,6 +44,8 @@ func _ready():
 			$"pivot/leftArm/brazo-esferaPlayer".show()
 		4:
 			$"pivot/leftArm/brazo-trianguloPlayer".show()
+		5:
+			$"pivot/leftArm/brazo-amebaPlayerMKII".show()
 	match rightArm:
 		0:
 			$"pivot/rightArm/brazo-amebaPlayer".show()
@@ -51,6 +57,8 @@ func _ready():
 			$"pivot/rightArm/brazo-esferaPlayer".show()
 		4:
 			$"pivot/rightArm/brazo-trianguloPlayer".show()
+		5:
+			$"pivot/rightArm/brazo-amebaPlayerMKII".show()
 	match legs:
 		0:
 			$pivot/legs/ameba.show()
@@ -62,6 +70,8 @@ func _ready():
 			$pivot/legs/spheres.show()
 		4:
 			$"pivot/legs/piernas-piramidePlayer".show()
+		5:
+			$pivot/legs/amebaEvil.show()
 		
 	
 	
