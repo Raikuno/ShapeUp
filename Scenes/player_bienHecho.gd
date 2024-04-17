0d1d78f163dc7d7d44a88dd43aea47fbb3b23ba8
Jorge Rojas Díaz <rdj160104@gmail.com>
2024-04-17 19:53:30 +0200

    animacionesYmapa

--- Scenes/player_bienHecho.gd
+++ Scenes/player_bienHecho.gd
@@ -1,31 +1,29 @@
 extends CharacterBody3D
+enum {MOVE, STATIC}
+enum {CUBE, PYRAMID, SPHERE, CYLINDER, AMEBA}
 
+var head
+var body
+var right
+var left
+var feet
+var state
+var animation
+var new_animation
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+func _ready():
+	pass
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	pass
+func changeState(newState):
+	state = newState
+	match state:
+		MOVE:
+			pass
+		STATIC:
+			pass
+func changePolygon(newPolygon):
+	polygon = newPolygon
+	match polygon:
+		
