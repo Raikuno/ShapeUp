523c07505cbcc55d4f42c7a60cc3578d3abda30b
Jorge Rojas Díaz <rdj160104@gmail.com>
2024-04-22 23:27:55 +0200

    La camara ha tenido más problemas que el matrimonio de mis padres

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,3 +3,6 @@ extends Node
 func _input(event):
 	if Input.is_action_just_pressed("pause"):
 		get_tree().quit()
+
+
+ 
--- Scenes/player_bienHecho.gd
+++ Scenes/player_bienHecho.gd
@@ -2,35 +2,72 @@ extends CharacterBody3D
 enum {MOVE, STATIC}
 enum {CUBE, PYRAMID, SPHERE, CYLINDER, AMEBA}
 enum {HEAD, BODY, RIGHT, LEFT, FEET}
-
-var head
-var body
-var right
-var left
-var feet
-var state
+#Partes del cuerpo
+@onready var head = AMEBA
+@onready var body = AMEBA
+@onready var right = AMEBA
+@onready var left = AMEBA
+@onready var feet = AMEBA
+@onready var state = STATIC
+#Animaciones 
 var animation
-
+var animation_feet
+var new_animation_feet
+var new_animation
+#Velocidad
+var target_velocity = Vector3.ZERO
+@export var speed = 8
+#Apuntado
+var rayOrigin = Vector3()
+var rayEnd = Vector3()
 func _ready():
 	changePolygon(AMEBA, HEAD)
 	changePolygon(AMEBA, LEFT)
 	changePolygon(AMEBA, RIGHT)
 	changePolygon(AMEBA, BODY)
 	changePolygon(AMEBA, FEET)
-
 func _physics_process(delta):
 	headLogic(delta)
 	bodyLogic(delta)
 	rightLogic(delta)
 	leftLogic(delta)
 	feetLogic(delta)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 2000
+	var from = $Camera3D2.project_ray_origin(mouse_pos)
+	var to = from + $Camera3D2.project_ray_normal(mouse_pos) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = from
+	ray_query.to = to
+	ray_query.collide_with_areas = true
+	var raycast_result = space.intersect_ray(ray_query)
+	
+	if not raycast_result.is_empty():
+		var pos = raycast_result.position
+		$pivot.look_at(Vector3(pos.x, position.y, pos.z), Vector3.UP)
+	#Coomprobaciones de animaciones
+	if animation != new_animation:
+		animation = new_animation
+		$body_animation.play(animation)
+	if animation_feet != new_animation_feet:
+		animation_feet = new_animation_feet
+		print(animation_feet)
+		$feet_animation.play(animation_feet)
+	#Comprbaciones de estado
+	if velocity != Vector3.ZERO and state == STATIC:
+		changeState(MOVE)
+	if velocity == Vector3.ZERO and state == MOVE:
+		changeState(STATIC)
+	#No se me ocurre otra etiqueta. Cosas, supongo
 func changeState(newState):
 	state = newState
 	match state:
 		MOVE:
-			pass
+			new_animation_feet = "feet"
 		STATIC:
-			pass
+			new_animation_feet = "idle"
+	print(new_animation_feet)
 func changePolygon(newPolygon, type):
 	match type:
 		HEAD:
@@ -52,4 +89,17 @@ func leftLogic(delta):
 func rightLogic(delta):
 	pass
 func feetLogic(delta):
+	var vectorDir = Vector3.ZERO
+	if Input.is_action_pressed("right"):
+		vectorDir.z += 1
+	if Input.is_action_pressed("left"):
+		vectorDir.z -= 1
+	if Input.is_action_pressed("down"):
+		vectorDir.x -= 1
+	if Input.is_action_pressed("up"):
+		vectorDir.x += 1
+	target_velocity.x = vectorDir.x * speed
+	target_velocity.z = vectorDir.z * speed
+	velocity = target_velocity
+	move_and_slide()
 	pass
